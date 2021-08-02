c02135bfb90187d3ece92c1e0edc30365143934c
BorbiukR <borbiuk.r@gmail.com>
2021-08-02 13:55:26 +0300

    project initialization and identity setting

---
+++ CardFile.WebAPI/Interfaces/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace CardFile.WebAPI.Interfaces
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string content);
+    }
+}
---
+++ CardFile.WebAPI/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using CardFile.WebAPI.Models;
+using System.Threading.Tasks;
+
+namespace CardFile.WebAPI.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserManagerResponse> RegisterUserAsync(Register model);
+
+        Task<UserManagerResponse> LoginUserAsync(Login model);
+
+        Task<UserManagerResponse> ConfirmEmailAsync(string userId, string token);
+
+        Task<UserManagerResponse> ForgetPasswordAsync(string email);
+
+        Task<UserManagerResponse> ResetPasswordAsync(ResetPassword model);
+    }
+}
